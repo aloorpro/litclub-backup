710bd77a119ffe8bc8c33c663b5bb4571731dff2
aloorpro <eliazavad@gmail.com>
2020-02-02 22:16:29 +0300

    http-requests decoded

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
-import Lib
+import GetFromXML
+import System.Environment
+import System.IO
+import Network.HTTP
+import Codec.Binary.UTF8.String
 
 main :: IO ()
-main = someFunc
+main = do
+    [f] <- getArgs
+    x <- simpleHTTP (getRequest f)
+    s <- decodeString <$> getResponseBody x
+    putStrLn $ getTitle s ++ "\n\n" ++ getText s
+
